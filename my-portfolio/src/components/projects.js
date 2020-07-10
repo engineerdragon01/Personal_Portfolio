1997d8f48e407453064b498128537a41b9df9625
Adam Chois <dragonboy174@gmail.com>
2020-07-10 00:51:14 -0700

    Finished layout for projects page, link projects later

--- my-portfolio/src/components/projects.js
+++ my-portfolio/src/components/projects.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Tabs, Tab, Grid, Cell } from 'react-mdl';
+import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardActions, Button, CardMenu, CardText, IconButton } from 'react-mdl';
 
 class Projects extends React.Component {
   constructor(props) {
@@ -12,15 +12,90 @@ class Projects extends React.Component {
   toggleCategories() {
     if (this.state.activeTab === 0) {
       return (
-        <div><h1>This is React</h1></div>
+        <div className="projects-grid">
+          {/*Project 1*/}
+          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+            <CardTitle style={{color: '#fff', height: '176px', background:
+            'url(https://i.pinimg.com/originals/42/c4/72/42c472e40153ad462c21ee940da50374.jpg) center / cover'}} >React Project #1</CardTitle>
+            <CardText>
+              A project I have been working on.
+            </CardText>
+            <CardActions border>
+              <Button colored>View Code</Button>
+            </CardActions>
+            <CardMenu style={{color: '#fff'}}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+
+          {/*Project 2*/}
+          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+            <CardTitle style={{color: '#fff', height: '176px', background:
+            'url(https://i.pinimg.com/originals/42/c4/72/42c472e40153ad462c21ee940da50374.jpg) center / cover'}} >React Project #1</CardTitle>
+            <CardText>
+              A project I have been working on.
+            </CardText>
+            <CardActions border>
+              <Button colored>View Code</Button>
+            </CardActions>
+            <CardMenu style={{color: '#fff'}}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+
+          {/*Project 3*/}
+          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+            <CardTitle style={{color: '#fff', height: '176px', background:
+            'url(https://i.pinimg.com/originals/42/c4/72/42c472e40153ad462c21ee940da50374.jpg) center / cover'}} >React Project #1</CardTitle>
+            <CardText>
+              A project I have been working on.
+            </CardText>
+            <CardActions border>
+              <Button colored>View Code</Button>
+            </CardActions>
+            <CardMenu style={{color: '#fff'}}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+        </div>
       )
     } else if (this.state.activeTab === 1) {
       return (
-        <div><h1>This is JavaScript</h1></div>
+        <div className="projects-grid">
+          {/*Project 1*/}
+          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+            <CardTitle style={{color: '#fff', height: '176px', background:
+            'url(https://i.pinimg.com/originals/42/c4/72/42c472e40153ad462c21ee940da50374.jpg) center / cover'}} >React Project #1</CardTitle>
+            <CardText>
+              A project I have been working on.
+            </CardText>
+            <CardActions border>
+              <Button colored>View Code</Button>
+            </CardActions>
+            <CardMenu style={{color: '#fff'}}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+        </div>
       )
     } else if (this.state.activeTab === 2) {
       return (
-        <div><h1>This is Python</h1></div>
+        <div className="projects-grid">
+          {/*Project 1*/}
+          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+            <CardTitle style={{color: '#fff', height: '176px', background:
+            'url(https://i.pinimg.com/originals/42/c4/72/42c472e40153ad462c21ee940da50374.jpg) center / cover'}} >React Project #1</CardTitle>
+            <CardText>
+              A project I have been working on.
+            </CardText>
+            <CardActions border>
+              <Button colored>View Code</Button>
+            </CardActions>
+            <CardMenu style={{color: '#fff'}}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+        </div>
       )
     }
   }
@@ -33,14 +108,13 @@ class Projects extends React.Component {
           <Tab>JavaScript</Tab>
           <Tab>Python</Tab>
         </Tabs>
-        <section className="projects-grid">
-          <Grid className="projects-grid">
+
+          <Grid>
             <Cell col={12}>
               <div className="content">{this.toggleCategories()}</div>
             </Cell>
           </Grid>
-          {this.toggleCategories()}
-        </section>
+
       </div>
     )
   }

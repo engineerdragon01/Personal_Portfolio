95e6c47b018ceb80cae33682ba08e6e743fc248a
Adam Chois <dragonboy174@gmail.com>
2020-07-11 01:23:59 -0700

    pushing through, spruce up later

--- my-portfolio/src/components/contact.js
+++ my-portfolio/src/components/contact.js
@@ -1,11 +1,45 @@
 import React from 'react';
-
+import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl';
 
 class Contact extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Contact Page</h1>
+      <div className="contact-body">
+        <Grid className="contact-grid">
+          <Cell col={6}>
+            <h2>Adam Chois</h2>
+            <img
+              src="https://cdn2.iconfinder.com/data/icons/avatar-2/512/Fred_man-512.png"
+              alt="avatar"
+              style={{height: '250px'}}
+              />
+            <p style={{width: '75%', margin: 'auto', paddingTop: '1em'}}>
+              Hi there! Thank you for visiting my website. I am a Bioengineering student
+              at UC Berkeley with a passion for Computer Science and Programming. Full Stack
+              Development is very fun for me as both an artist and a future engineer.
+            </p>
+          </Cell>
+          <Cell col={6}>
+            <h2>Contact Me</h2>
+            <hr/>
+            <div className="contact-list">
+            <List>
+              <ListItem>
+                <ListItemContent style={{fontSize: '25px', fontFamily: 'Anton' }}>
+                  <i className="fa fa-phone-square" aria-hidden="true"/>
+                  (916) 690 1344
+                </ListItemContent>
+              </ListItem>
+              <ListItem>
+                <ListItemContent style={{fontSize: '25px', fontFamily: 'Anton' }}>
+                  <i className="fa fa-envelope" aria-hidden="true"/>
+                  engineerdragon01@berkeley.edu
+                </ListItemContent>
+              </ListItem>
+            </List>
+            </div>
+          </Cell>
+        </Grid>
       </div>
     )
   }
--- my-portfolio/src/components/resume.js
+++ my-portfolio/src/components/resume.js
@@ -1,11 +1,38 @@
 import React from 'react';
-
+import { Grid, Cell } from 'react-mdl';
+import Education from './education';
 
 class Resume extends React.Component {
   render() {
     return (
       <div>
-        <h1>Resume Page</h1>
+        <Grid>
+          <Cell col={4}>
+            <div style={{textAlign: 'center'}}>
+              <img
+                src="https://www.shareicon.net/download/2015/09/18/103157_man_512x512.png"
+                alt="avatar"
+                style={{height: '200px'}}
+                />
+            </div>
+            <h2 style={{paddinTop: '2em'}}>Adam Chois</h2>
+            <h4 style={{color: 'grey'}}>Programmer</h4>
+            <hr style={{borderTop: '3px solid #833fb2', width: '50%'}} />
+            <p>Put some text here</p>
+            <hr style={{borderTop: '3px solid #833fb2', width: '50%'}} />
+            <h5>Address</h5>
+            <p>132 Hacker Way Menlo Park, 94035</p>
+            <h5>Phone</h5>
+            <p>(916) 690 1344</p>
+            <h5>Email</h5>
+            <p>engineerdragon01@berkeley.edu</p>
+            <hr style={{borderTop: '3px solid #833fb2', width: '50%'}} />
+          </Cell>
+          <Cell col={8} className="resume-right-col">
+            <h2>Education</h2>
+            <Education />
+          </Cell>
+        </Grid>
       </div>
     )
   }
